1c1be91f8032fc6c4130c405db9050a506bf6b36
ianlavine <71903686+ianlavine@users.noreply.github.com>
2021-12-05 21:42:18 -0500

    base template for index and ranks html

--- app/main/routes.py
+++ app/main/routes.py
@@ -21,11 +21,14 @@ ordered = []
 to_show = None
 sort_albs = []
 
+
 @bp.route("/ranks", methods=['GET', 'POST'])
 @login_required
 def ranks():
 
     global ordered, to_show, sort_albs
+
+    newform, artistform = form_checks()
 
     user = User.query.filter_by(username=current_user.username).first_or_404()
     artistform = update_artist_form(user)
@@ -57,7 +60,7 @@ def ranks():
         ordered = [{'name': x.name, 'score': int(x.score)} for x in temp]
 
 
-    return render_template('main/ranks.html', artistform=artistform, sort_albs=sort_albs, topten=topten, art=artist, ordered=ordered, to_show=to_show)
+    return render_template('main/ranks.html', artistform=artistform, newform=newform, sort_albs=sort_albs, topten=topten, art=artist, ordered=ordered, to_show=to_show)
 
 
 @bp.route("/index", methods=['GET', 'POST'])
@@ -65,6 +68,61 @@ def ranks():
 def index():
 
     global game_songs, album_data, album_data_off, song_data, topten, artist, images
+
+    newform, artistform = form_checks()
+
+    alber = request.args.get("alber", "")
+    alber2 = request.args.get("alber2", "")
+    to_change = None
+    if alber or alber2:
+        if alber2:
+            to_change = album_data_off[int(alber2)]
+        elif len(album_data) > 1:
+            to_change = album_data[int(alber)]
+    if to_change != None:
+        to_change.swap()
+        db.session.add(artist)
+        db.session.add(to_change)
+        db.session.commit()
+        update_albums()
+        game_songs = Ranking.new_battle(song_data)
+
+    sonny = request.args.get("songer", "")
+    if sonny and artist:
+        if int(sonny) == 0:
+            Ranking.compare(game_songs[0], game_songs[1])
+        if int(sonny) == 1:
+            Ranking.compare(game_songs[1], game_songs[0])
+        
+        user.rounds += 1
+        artist.rounds += 1
+
+        db.session.add(user)
+        db.session.add(artist)
+        db.session.add(game_songs[0])
+        db.session.add(game_songs[1])
+        db.session.commit()
+
+        song_data.sort()
+        topten = [{'name': x.name, 'score': int(x.score)} for x in song_data]
+
+        game_songs = Ranking.new_battle(song_data)
+
+    if game_songs[0] != None and artist != None:
+        for s in range(len(game_songs)):
+            db.session.add(game_songs[s])
+            db.session.commit()
+            db.session.add(game_songs[s].owner)
+            db.session.commit()
+            images[s] = game_songs[s].owner.cover
+            db.session.commit()
+
+    return render_template('main/index.html', album_data=album_data, album_data_off=album_data_off, game_songs=game_songs, images=images, newform=newform, artistform=artistform, topten=topten, art=artist)
+
+
+def form_checks():
+
+    global user, artist, game_songs
 
     user = User.query.filter_by(username=current_user.username).first_or_404()
     artistform = update_artist_form(user)
@@ -92,16 +150,14 @@ def index():
 
         if artistform.submit.data:
             artist = Artist.query.filter_by(id=artistform.select_artist.data).first_or_404()
-            update_data(artistform)
+            update_albums()
             game_songs = Ranking.new_battle(song_data)
 
         elif artistform.remove.data:
 
             temp_artist = Artist.query.filter_by(id=artistform.select_artist.data).first_or_404()
 
-            if artist == temp_artist:
-                artist = None
-                update_data(artistform)
+            artist = None
 
             for alb in temp_artist.albums.all():
                 for s in alb.songs.all():
@@ -110,51 +166,9 @@ def index():
             db.session.delete(temp_artist)
             db.session.commit()
 
-    alber = request.args.get("alber", "")
-    alber2 = request.args.get("alber2", "")
-    if alber or alber2:
-        if alber:
-            to_change = album_data[int(alber)]
-        else:
-            to_change = album_data_off[int(alber2)]
-        to_change.swap()
-        db.session.add(artist)
-        db.session.add(to_change)
-        db.session.commit()
-        update_albums()
-        game_songs = Ranking.new_battle(song_data)
+            artistform = update_artist_form(user)
 
-    sonny = request.args.get("songer", "")
-    if sonny:
-        if int(sonny) == 0:
-            Ranking.compare(game_songs[0], game_songs[1])
-        if int(sonny) == 1:
-            Ranking.compare(game_songs[1], game_songs[0])
-        
-        user.rounds += 1
-        artist.rounds += 1
-
-        db.session.add(user)
-        db.session.add(artist)
-        db.session.add(game_songs[0])
-        db.session.add(game_songs[1])
-        db.session.commit()
-
-        song_data.sort()
-        topten = [{'name': x.name, 'score': int(x.score)} for x in song_data]
-
-        game_songs = Ranking.new_battle(song_data)
-
-    if game_songs[0] != None:
-        for s in range(len(game_songs)):
-            db.session.add(game_songs[s])
-            db.session.commit()
-            db.session.add(game_songs[s].owner)
-            db.session.commit()
-            images[s] = game_songs[s].owner.cover
-            db.session.commit()
-
-    return render_template('main/index.html', album_data=album_data, album_data_off=album_data_off, game_songs=game_songs, images=images, newform=newform, artistform=artistform, topten=topten, art=artist)
+    return (newform, artistform)
 
 
 def update_data(form):
