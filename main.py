5f95b982e74c11f7399fbc2cceaa197fe2f54f1d
Umer Kazi <umerkazi@hotmail.com>
2023-09-13 13:17:59 -0400

    clean

--- main.py
+++
@@ -1,8 +0,0 @@
-from app import create_app, db
-from app.models import User, Artist, Album, Song
-
-app = create_app()
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'User': User, 'Artist': Artist, 'Album': Album, 'Song': Song}
